211ba232aec7f0e0ee9a3b51e4ef2b0cca777daf
ingrid soto <ingridsoto.prog@gmail.com>
2024-01-03 09:44:01 -0300

    feat: add login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,23 @@
+import { Input } from '@/components/input'
 import styles from './styles.scss'
 
 function LoginPage() {
+  function onSubmit() {
+    console.log('login')
+  }
+
   return (
-  <div className={styles['p-login']}>
-    <h1>Login Page</h1>
-  </div>)
+    <div className={styles['p-login']}>
+      <div className={styles['p-login__form-wrapper']}>
+        <h1 className={styles['p-login__form-title']}>Login</h1>
+        <form className={styles['p-login__form']} onSubmit={onSubmit}>
+          <Input placeholder="Username" type="text" />
+          <Input placeholder="Password" type="password" />
+          <button type="submit">Sign In</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default LoginPage
