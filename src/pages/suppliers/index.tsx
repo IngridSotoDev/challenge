4d9c3a97981abcc60cbc975f8bad81b9f0cca714
ingrid soto <ingridsoto.prog@gmail.com>
2024-01-03 12:02:16 -0300

    feat: add page suppliers

--- src/pages/suppliers/index.tsx
+++ src/pages/suppliers/index.tsx
@@ -1,5 +1,56 @@
+import { NavLink } from 'react-router-dom'
+import styles from './styles.scss'
+
 function SupplierPage() {
-  return <h1>SupplierPage</h1>
+  return (
+    <div className={styles['p-suppliers']}>
+      <header>
+        <h1>Suppliers</h1>
+      </header>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>CNPJ</th>
+            <th>Phone Number</th>
+            <th>Owner</th>
+            <th>Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Distribuidora A</td>
+            <td>99.999.999/9999-99</td>
+            <td>(99) 99999-9999</td>
+            <td>Elias</td>
+            <td>
+              <NavLink
+                className={styles['p-suppliers__navlink']}
+                to={'/suppliers/asdasdasdasd'}
+              >
+                Edit
+              </NavLink>
+            </td>
+          </tr>
+          <tr>
+            <td>Distribuidora A</td>
+            <td>99.999.999/9999-99</td>
+            <td>(99) 99999-9999</td>
+            <td>Elias</td>
+            <td>
+              <NavLink
+                className={styles['p-suppliers__navlink']}
+                to={'/suppliers/asdasdasdasd'}
+              >
+                Edit
+              </NavLink>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default SupplierPage
