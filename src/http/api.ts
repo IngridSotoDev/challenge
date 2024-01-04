c0dbaf93bca183c0a22a681768b121b558cf7c16
ingrid soto <ingridsoto.prog@gmail.com>
2024-01-04 17:33:10 -0300

    feat: add token in request header

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { Cookies } from 'react-cookie'
 
 export const api = axios.create({
   baseURL: process.env.API_URL,
@@ -10,4 +11,15 @@ export const apiToken = axios.create({
     username: process.env.API_AUTH_USERNAME,
     password: process.env.API_AUTH_PASSWORD,
   },
+})
+
+api.interceptors.request.use((request) => {
+  const cookies = new Cookies()
+  const token = cookies.get('auth_token')
+
+  if (token) {
+    request.headers.Authorization = `Bearer ${token}`
+  }
+
+  return request
 })
